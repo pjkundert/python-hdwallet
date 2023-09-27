30d3c5aaf324c75d5dce218c4cc06ccc95c78788
meherett <meherett@zoho.com>
2023-09-27 19:05:29 +0300

    Update: Ethereum address encoder and decoder

--- hdwallet/addresses/ethereum.py
+++ hdwallet/addresses/ethereum.py
@@ -3,11 +3,12 @@
 from typing import (
     Any, Union
 )
-from Crypto.Hash import keccak
 
 from ..ecc import (
     IPublicKey, SLIP10Secp256k1PublicKey
 )
+from ..crypto import kekkak256
+from ..utils import bytes_to_string
 from . import (
     IAddress, validate_and_get_public_key
 )
@@ -20,11 +21,11 @@ class EthereumAddress(IAddress):
     @staticmethod
     def checksum_encode(address: str) -> str:
         output: str = ""
-        keccak_256 = keccak.new(digest_bits=256)
-        keccak_256.update(address.lower().encode())
-        hash_addr = keccak_256.hexdigest()
+        address_hash: str = bytes_to_string(
+            kekkak256(address.lower())
+        )
         for i, c in enumerate(address):
-            if int(hash_addr[i], 16) >= 8:
+            if int(address_hash[i], 16) >= 8:
                 output += c.upper()
             else:
                 output += c
@@ -36,9 +37,9 @@ class EthereumAddress(IAddress):
         public_key: SLIP10Secp256k1PublicKey = validate_and_get_public_key(
             public_key=public_key, public_key_cls=SLIP10Secp256k1PublicKey
         )
-        keccak_256 = keccak.new(digest_bits=256)
-        keccak_256.update(public_key.raw_uncompressed()[1:])
-        address = keccak_256.hexdigest()[24:]
+        address: str = bytes_to_string(
+            kekkak256(public_key.raw_uncompressed()[1:])
+        )[24:]
         return cls.address_prefix + (
             address if kwargs.get("skip_checksum_encode", False) else cls.checksum_encode(address)
         )
@@ -48,11 +49,11 @@ class EthereumAddress(IAddress):
 
         address_prefix_got: str = address[:len(cls.address_prefix)]
         if cls.address_prefix != address_prefix_got:
-            raise ValueError(f"Invalid address_prefix (expected {cls.address_prefix!r}, got {address_prefix_got!r})")
+            raise ValueError(f"Invalid address_prefix (expected: {cls.address_prefix}, got: {address_prefix_got})")
         address_no_prefix = address[len(cls.address_prefix):]
 
         if len(address_no_prefix) != 40:
-            raise ValueError(f"Invalid length (expected {40}, got {len(address_no_prefix)})")
+            raise ValueError(f"Invalid length (expected: {40}, got: {len(address_no_prefix)})")
         # Check checksum encoding
         if not kwargs.get("skip_checksum_encode", False) and address_no_prefix != cls.checksum_encode(address_no_prefix):
             print(address_no_prefix, cls.checksum_encode(address_no_prefix))
