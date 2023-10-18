221b5cc661599b1cc14303e75c129d22b7cfbe9c
meherett <meherett.batu@gmail.com>
2023-10-18 18:53:25 +0300

    Update: copyright year of all mnemonic generators

--- hdwallet/seeds/__init__.py
+++ hdwallet/seeds/__init__.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 
+# Copyright © 2020-2023, Meheret Tesfaye Batu <meherett.batu@gmail.com>
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or https://opensource.org/license/mit
+
 from abc import (
     ABC, abstractmethod
 )
--- hdwallet/seeds/monero.py
+++ hdwallet/seeds/monero.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 
+# Copyright © 2020-2023, Meheret Tesfaye Batu <meherett.batu@gmail.com>
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or https://opensource.org/license/mit
+
 from ..mnemonics.monero import MoneroMnemonic
 from . import ISeed
 
@@ -7,7 +11,7 @@ from . import ISeed
 class MoneroSeed(ISeed):
 
     @classmethod
-    def generate(cls, mnemonic: str) -> str:
+    def generate(cls, mnemonic: str, **kwargs) -> str:
 
         if not MoneroMnemonic.is_valid(mnemonic=mnemonic):
             ValueError("Invalid Monero mnemonic words")
