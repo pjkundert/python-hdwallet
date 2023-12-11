987a5db4edbc08af05d1991dccbdbcfa9670c266
meherett <meherett.batu@gmail.com>
2023-12-11 06:04:38 +0300

    Add: Bitcoin cryptocurrency including interface

---
+++ hdwallet/cryptocurrencies/bitcoin.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+
+# Copyright © 2020-2024, Meheret Tesfaye Batu <meherett.batu@gmail.com>
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or https://opensource.org/license/mit
+
+from ..ecc import SLIP10Secp256k1
+from .icryptocurrency import (
+    CoinType, Cryptocurrency, SegwitAddress, Secp65k1Network, ExtendedPrivateKey, ExtendedPublicKey
+)
+
+
+class Mainnet(Secp65k1Network):
+
+    PUBLIC_KEY_ADDRESS_PREFIX = 0x00
+    SCRIPT_ADDRESS_PREFIX = 0x05
+    SEGWIT_ADDRESS_PREFIX = SegwitAddress({
+        "HRP": "bc",
+        "VERSION": 0x00
+    })
+    DEFAULT_PATH = f"m/44'/0'/0'/0/0"
+    EXTENDED_PRIVATE_KEY = ExtendedPrivateKey({
+        "P2PKH": 0x0488ade4,
+        "P2SH": 0x0488ade4,
+        "P2WPKH": 0x04b2430c,
+        "P2WPKH_IN_P2SH": 0x049d7878,
+        "P2WSH": 0x02aa7a99,
+        "P2WSH_IN_P2SH": 0x0295b005
+    })
+    EXTENDED_PUBLIC_KEY = ExtendedPublicKey({
+        "P2PKH": 0x0488b21e,
+        "P2SH": 0x0488b21e,
+        "P2WPKH": 0x04b24746,
+        "P2WPKH_IN_P2SH": 0x049d7cb2,
+        "P2WSH": 0x02aa7ed3,
+        "P2WSH_IN_P2SH": 0x0295b43f
+    })
+    WIF_PREFIX = 0x80
+
+
+class Testnet(Secp65k1Network):
+
+    PUBLIC_KEY_ADDRESS_PREFIX = 0x6f
+    SCRIPT_ADDRESS_PREFIX = 0xc4
+    SEGWIT_ADDRESS_PREFIX = SegwitAddress({
+        "HRP": "tb",
+        "VERSION": 0x00
+    })
+    DEFAULT_PATH = f"m/44'/1'/0'/0/0"
+    EXTENDED_PRIVATE_KEY = ExtendedPrivateKey({
+        "P2PKH": 0x04358394,
+        "P2SH": 0x04358394,
+        "P2WPKH": 0x045f18bc,
+        "P2WPKH_IN_P2SH": 0x044a4e28,
+        "P2WSH": 0x02575048,
+        "P2WSH_IN_P2SH": 0x024285b5
+    })
+    EXTENDED_PUBLIC_KEY = ExtendedPublicKey({
+        "P2PKH": 0x043587cf,
+        "P2SH": 0x043587cf,
+        "P2WPKH": 0x045f1cf6,
+        "P2WPKH_IN_P2SH": 0x044a5262,
+        "P2WSH": 0x02575483,
+        "P2WSH_IN_P2SH": 0x024289ef
+    })
+    WIF_PREFIX = 0xef
+
+
+class Networks:
+
+    MAINNET = Mainnet
+    TESTNET = Testnet
+
+
+class Bitcoin(Cryptocurrency):
+
+    NAME = "Bitcoin"
+    SYMBOL = "BTC"
+    SOURCE_CODE = "https://github.com/bitcoin/bitcoin"
+    ECC = SLIP10Secp256k1
+    NETWORKS = Networks
+    COIN_TYPE = CoinType({
+        "INDEX": 0,
+        "HARDENED": True
+    })
+    MESSAGE_PREFIX = "\x18Bitcoin Signed Message:\n"
---
+++ hdwallet/cryptocurrencies/icryptocurrency.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+
+# Copyright © 2020-2024, Meheret Tesfaye Batu <meherett.batu@gmail.com>
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or https://opensource.org/license/mit
+
+from types import SimpleNamespace
+from typing import (
+    Any, Optional
+)
+
+import inspect
+import sys
+
+from ..ecc import EllipticCurveCryptography
+
+
+class NestedNamespace(SimpleNamespace):
+
+    def __init__(self, dictionary, **kwargs):
+        super().__init__(**kwargs)
+        for key, value in dictionary.items():
+            if isinstance(value, dict):
+                self.__setattr__(key, NestedNamespace(value))
+            else:
+                self.__setattr__(key, value)
+
+
+class CoinType(NestedNamespace):
+
+    INDEX: int
+    HARDENED: bool
+
+    def __str__(self):
+        return f"{self.INDEX}'" if self.HARDENED else f"{self.INDEX}"
+
+
+class SegwitAddress(NestedNamespace):
+
+    HRP: Optional[str]
+    VERSION: int
+
+
+class ExtendedKey(NestedNamespace):
+
+    P2PKH: int
+    P2SH: int
+
+    P2WPKH: Optional[int] = None
+    P2WPKH_IN_P2SH: Optional[int] = None
+
+    P2WSH: Optional[int] = None
+    P2WSH_IN_P2SH: Optional[int] = None
+
+
+class ExtendedPrivateKey(ExtendedKey):
+    pass
+
+
+class ExtendedPublicKey(ExtendedKey):
+    pass
+
+
+class Secp65k1Network(NestedNamespace):
+
+    PUBLIC_KEY_ADDRESS_PREFIX: int
+    SCRIPT_ADDRESS_PREFIX: int
+    SEGWIT_ADDRESS_PREFIX: SegwitAddress
+    DEFAULT_PATH: str
+    EXTENDED_PRIVATE_KEY: ExtendedPrivateKey
+    EXTENDED_PUBLIC_KEY: ExtendedPublicKey
+    WIF_PREFIX: int
+
+
+class Cryptocurrency(NestedNamespace):
+
+    NAME: str
+    SYMBOL: str
+    NETWORKS: Any
+    SOURCE_CODE: Optional[str]
+    ECC: EllipticCurveCryptography
+    COIN_TYPE: CoinType
+    MESSAGE_PREFIX: Optional[str]
+
+    @classmethod
+    def get_network(cls, network: str) -> Any:
+        return getattr(cls.NETWORKS, network)
+
+
+def get_cryptocurrency(symbol: str) -> Cryptocurrency:
+
+    for _, cryptocurrency in inspect.getmembers(sys.modules[__name__]):
+        if inspect.isclass(cryptocurrency):
+            if issubclass(cryptocurrency, Cryptocurrency) and cryptocurrency != Cryptocurrency:
+                if symbol == cryptocurrency.SYMBOL:
+                    return cryptocurrency
+
+    raise ValueError(f"Invalid Cryptocurrency '{symbol}' symbol")
