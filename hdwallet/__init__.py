6f89d477fa8df1986c79e2eb0fe5a3045ea4685b
meherett <meherett.batu@gmail.com>
2024-03-05 23:19:52 +0300

    Update: HDWallet init values

--- hdwallet/__init__.py
+++ hdwallet/__init__.py
@@ -1,32 +1,28 @@
 #!/usr/bin/env python3
 
-from .hdwallet import (
-    HDWallet,
-    BIP32HDWallet,
-    BIP44HDWallet,
-    BIP49HDWallet,
-    BIP84HDWallet,
-    BIP141HDWallet
-)
+# Copyright © 2020-2024, Meheret Tesfaye Batu <meherett.batu@gmail.com>
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or https://opensource.org/license/mit
 
-# HDWallet Information's
-__version__: str = "v2.2.1"
+from .hdwallet import HDWallet
+
+__version__: str = "v3.0.0"
 __license__: str = "MIT"
 __author__: str = "Meheret Tesfaye Batu"
 __email__: str = "meherett.batu@gmail.com"
+__documentation__: str = "https://hdwallet.readthedocs.com"
 __description__: str = "Python-based library for the implementation of a hierarchical deterministic wallet " \
-                       "generator for more than 140+ multiple cryptocurrencies."
+                       "generator for more than 150+ multiple cryptocurrencies."
+__website__: str = "https://hdwallet.online"
+
 
 __all__: list = [
     "__version__",
     "__license__",
     "__author__",
     "__email__",
+    "__documentation__",
     "__description__",
-    "HDWallet",
-    "BIP32HDWallet",
-    "BIP44HDWallet",
-    "BIP49HDWallet",
-    "BIP84HDWallet",
-    "BIP141HDWallet"
+    "__website__",
+    "HDWallet"
 ]
