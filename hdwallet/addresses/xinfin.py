779d6d10da75d9684209166eea3c733617414f0a
meherett <meherett.batu@gmail.com>
2024-03-05 23:20:49 +0300

    Add: XinFin address implementation

--- hdwallet/addresses/xinfin.py
+++ hdwallet/addresses/xinfin.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Copyright © 2023, Meheret Tesfaye Batu <meherett.batu@gmail.com>
+# Copyright © 2020-2024, Meheret Tesfaye Batu <meherett.batu@gmail.com>
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or https://opensource.org/license/mit
 
@@ -10,3 +10,7 @@ from .ethereum import EthereumAddress
 class XinFinAddress(EthereumAddress):
 
     address_prefix: str = "xdc"
+
+    @staticmethod
+    def name() -> str:
+        return "XinFin"
