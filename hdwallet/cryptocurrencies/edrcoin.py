2922501411fee031e91272965354b93994793121
meherett <meherett.batu@gmail.com>
2024-03-05 23:08:12 +0300

    Add: EDR-Coin cryptocurrency and address

---
+++ hdwallet/cryptocurrencies/edrcoin.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+
+# Copyright © 2020-2024, Meheret Tesfaye Batu <meherett.batu@gmail.com>
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or https://opensource.org/license/mit
+
+from ..ecc import SLIP10Secp256k1ECC
+from ..const import (
+    CoinType, Entropies, Mnemonics, Seeds, HDs, Addresses, Networks, XPrivateKeyVersions, XPublicKeyVersions
+)
+from .icryptocurrency import (
+    ICryptocurrency, INetwork
+)
+
+
+class Mainnet(INetwork):
+
+    PUBLIC_KEY_ADDRESS_PREFIX = 0x5d
+    SCRIPT_ADDRESS_PREFIX = 0x1c
+    XPRIVATE_KEY_VERSIONS = XPrivateKeyVersions({
+        "P2PKH": 0x488ade4,
+        "P2SH": 0x488ade4
+    })
+    XPUBLIC_KEY_VERSIONS = XPublicKeyVersions({
+        "P2PKH": 0x488b21e,
+        "P2SH": 0x488b21e
+    })
+    MESSAGE_PREFIX = "\x18EDRcoin Signed Message:\n"
+    WIF_PREFIX = 0xdd
+
+
+class EDRCoin(ICryptocurrency):
+
+    NAME = "EDR Coin"
+    SYMBOL = "EDRC"
+    SOURCE_CODE = "https://github.com/EDRCoin/EDRcoin-src"
+    ECC = SLIP10Secp256k1ECC
+    COIN_TYPE = CoinType({
+        "INDEX": 56,
+        "HARDENED": True
+    })
+    NETWORKS = Networks({
+        "MAINNET": Mainnet
+    })
+    DEFAULT_NETWORK = NETWORKS.MAINNET
+    ENTROPIES = Entropies({
+        "BIP39"
+    })
+    MNEMONICS = Mnemonics({
+        "BIP39"
+    })
+    SEEDS = Seeds({
+        "BIP39"
+    })
+    HDS = HDs({
+        "BIP32", "BIP44"
+    })
+    DEFAULT_HD = HDS.BIP44
+    ADDRESSES = Addresses({
+        "P2PKH", "P2SH"
+    })
+    DEFAULT_ADDRESS = ADDRESSES.P2PKH
