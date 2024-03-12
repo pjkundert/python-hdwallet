b5bbf94266c3dbc8c77966fb746c81f9400db87c
meherett <meherett.batu@gmail.com>
2024-03-12 11:23:58 +0300

    Add: Filecoin (FIL) cryptocurrency and address

--- hdwallet/addresses/filecoin.py
+++ hdwallet/addresses/filecoin.py
@@ -1,62 +1,74 @@
 #!/usr/bin/env python3
 
-from binascii import unhexlify
+# Copyright © 2020-2024, Meheret Tesfaye Batu <meherett.batu@gmail.com>
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or https://opensource.org/license/mit
+
 from typing import (
     Any, Union
 )
-
-import hashlib
 
 from ..libs.base32 import (
     encode_no_padding, decode
 )
 from ..ecc import (
-    IPublicKey, SLIP10Secp256k1PublicKey
+    IPublicKey, SLIP10Secp256k1PublicKey, validate_and_get_public_key
 )
-from ..utils import integer_to_bytes
-from . import (
-    IAddress, validate_and_get_public_key
+from ..cryptocurrencies import Filecoin
+from ..crypto import (
+    blake2b_32, blake2b_160
 )
+from ..utils import (
+    get_bytes, integer_to_bytes, bytes_to_string
+)
+from ..exceptions import AddressTypeError
+from .iaddress import IAddress
 
 
 class FilecoinAddress(IAddress):
 
-    alphabet: str = "abcdefghijklmnopqrstuvwxyz234567"
-    address_prefix: str = "f"
+    alphabet: str = Filecoin.PARAMS.ALPHABET
+    address_prefix: str = Filecoin.PARAMS.ADDRESS_PREFIX
     address_types: dict = {
-        "secp256k1": 1,  # A SECP256K1 public key address.
-        "bls": 3  # A BLS public key address.
+        "secp256k1": Filecoin.PARAMS.ADDRESS_TYPES.SECP256K1,
+        "bls": Filecoin.PARAMS.ADDRESS_TYPES.BLS
     }
+
+    @staticmethod
+    def name() -> str:
+        return "Filecoin"
 
     @classmethod
     def compute_checksum(cls, public_key_hash: bytes, address_type: int) -> bytes:
-        return hashlib.blake2b(
-            (integer_to_bytes(address_type) + public_key_hash), digest_size=4
-        ).digest()
+        return blake2b_32(
+            integer_to_bytes(address_type) + public_key_hash
+        )
 
     @classmethod
     def encode(cls, public_key: Union[bytes, str, IPublicKey], **kwargs: Any) -> str:
 
-        public_key: SLIP10Secp256k1PublicKey = validate_and_get_public_key(
+        public_key: IPublicKey = validate_and_get_public_key(
             public_key=public_key, public_key_cls=SLIP10Secp256k1PublicKey
         )
-        public_key_hash: bytes = hashlib.blake2b(
-            public_key.raw_uncompressed(), digest_size=20
-        ).digest()
+        public_key_hash: bytes = blake2b_160(
+            public_key.raw_uncompressed()
+        )
 
-        if not kwargs.get("address_type") or \
-                kwargs.get("address_type") == "secp256k1":
-            address_type: int = cls.address_types["secp256k1"]
-        elif kwargs.get("address_type") == "bls":
-            address_type: int = cls.address_types["bls"]
+        if not kwargs.get("address_type"):
+            address_type: int = cls.address_types[Filecoin.PARAMS.DEFAULT_ADDRESS_TYPE]
         else:
-            raise ValueError("Invalid filecoin address type")
+            if kwargs.get("address_type") not in Filecoin.ADDRESS_TYPES.get_address_types():
+                raise AddressTypeError(
+                    f"Invalid {cls.name()} address type",
+                    expected=Filecoin.ADDRESS_TYPES.get_address_types(),
+                    got=kwargs.get("address_type")
+                )
+            address_type: int = cls.address_types[kwargs.get("address_type")]
 
         checksum: bytes = cls.compute_checksum(public_key_hash, address_type)
         base32_encode: str = encode_no_padding(
             (public_key_hash + checksum).hex(), cls.alphabet
         )
-
         return (
             cls.address_prefix + chr(address_type + ord("0")) + base32_encode
         )
@@ -66,31 +78,34 @@ class FilecoinAddress(IAddress):
 
         prefix_got: str = address[:len(cls.address_prefix)]
         if cls.address_prefix != prefix_got:
-            raise ValueError(f"Invalid prefix (expected {cls.address_prefix!r}, got {prefix_got!r})")
+            raise ValueError(f"Invalid prefix (expected: {cls.address_prefix}, got: {prefix_got})")
         address_no_prefix: str = address[len(cls.address_prefix):]
 
-        if not kwargs.get("address_type") or \
-                kwargs.get("address_type") == "secp256k1":
-            address_type: int = cls.address_types["secp256k1"]
-        elif kwargs.get("address_type") == "bls":
-            address_type: int = cls.address_types["bls"]
+        if not kwargs.get("address_type"):
+            address_type: int = cls.address_types[Filecoin.PARAMS.DEFAULT_ADDRESS_TYPE]
         else:
-            raise ValueError("Invalid filecoin address type")
+            if kwargs.get("address_type") not in Filecoin.ADDRESS_TYPES.get_address_types():
+                raise AddressTypeError(
+                    f"Invalid {cls.name()} address type",
+                    expected=Filecoin.ADDRESS_TYPES.get_address_types(),
+                    got=kwargs.get("address_type")
+                )
+            address_type: int = cls.address_types[kwargs.get("address_type")]
 
         address_type_got = ord(address_no_prefix[0]) - ord("0")
         if address_type != address_type_got:
-            raise ValueError(f"Invalid address type (expected {address_type}, got {address_type_got})")
+            raise ValueError(f"Invalid address type (expected: {address_type}, got: {address_type_got})")
 
-        address_decode: bytes = unhexlify(decode(address_no_prefix[1:], cls.alphabet))
+        address_decode: bytes = get_bytes(decode(address_no_prefix[1:], cls.alphabet))
 
         if len(address_decode) != 24:
-            raise ValueError(f"Invalid length (expected {24}, got {len(address_decode)})")
+            raise ValueError(f"Invalid length (expected: {24}, got: {len(address_decode)})")
 
         checksum: bytes = address_decode[-1 * 4:]
         public_key_hash: bytes = address_decode[:-1 * 4]
 
         checksum_got: bytes = cls.compute_checksum(public_key_hash, address_type)
         if checksum != checksum_got:
-            raise ValueError(f"Invalid checksum (expected {checksum.hex()}, got {checksum_got.hex()})")
+            raise ValueError(f"Invalid checksum (expected: {checksum.hex()}, got: {checksum_got.hex()})")
 
-        return public_key_hash.hex()
+        return bytes_to_string(public_key_hash)
