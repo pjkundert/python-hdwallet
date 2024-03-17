d82a227e7ef018c9e38b531499dc70f2fd3c97e2
meherett <meherett.batu@gmail.com>
2024-03-17 21:32:46 +0300

    Modify: Elliptic Curve Cryptography (ECC) imports

--- hdwallet/ecc/__init__.py
+++ hdwallet/ecc/__init__.py
@@ -24,12 +24,12 @@ from .slip10 import (
 )
 
 ECCS: Dict[str, Type[IEllipticCurveCryptography]] = {
-    "Kholaw-Ed25519": KholawEd25519ECC,
-    "SLIP10-Ed25519": SLIP10Ed25519ECC,
-    "SLIP10-Ed25519-Blake2b": SLIP10Ed25519Blake2bECC,
-    "SLIP10-Ed25519-Monero": SLIP10Ed25519MoneroECC,
-    "SLIP10-Nist256p1": SLIP10Nist256p1ECC,
-    "SLIP10-Secp256k1": SLIP10Secp256k1ECC
+    KholawEd25519ECC.NAME: KholawEd25519ECC,
+    SLIP10Ed25519ECC.NAME: SLIP10Ed25519ECC,
+    SLIP10Ed25519Blake2bECC.NAME: SLIP10Ed25519Blake2bECC,
+    SLIP10Ed25519MoneroECC.NAME: SLIP10Ed25519MoneroECC,
+    SLIP10Nist256p1ECC.NAME: SLIP10Nist256p1ECC,
+    SLIP10Secp256k1ECC.NAME: SLIP10Secp256k1ECC
 }
 
 
@@ -50,11 +50,13 @@ def validate_and_get_public_key(
 
 __all__: List[str] = [
     "IPoint", "IPublicKey", "IPrivateKey", "IEllipticCurveCryptography",
-    "KholawEd25519ECC", "KholawEd25519Point", "KholawEd25519PublicKey", "KholawEd25519PrivateKey",
-    "SLIP10Ed25519ECC", "SLIP10Ed25519Point", "SLIP10Ed25519PublicKey", "SLIP10Ed25519PrivateKey",
-    "SLIP10Ed25519Blake2bECC", "SLIP10Ed25519Blake2bPoint", "SLIP10Ed25519Blake2bPublicKey", "SLIP10Ed25519Blake2bPrivateKey",
-    "SLIP10Ed25519MoneroECC", "SLIP10Ed25519MoneroPoint", "SLIP10Ed25519MoneroPublicKey", "SLIP10Ed25519MoneroPrivateKey",
-    "SLIP10Nist256p1ECC", "SLIP10Nist256p1Point", "SLIP10Nist256p1PublicKey", "SLIP10Nist256p1PrivateKey",
-    "SLIP10Secp256k1ECC", "SLIP10Secp256k1Point", "SLIP10Secp256k1PublicKey", "SLIP10Secp256k1PrivateKey",
+    "KholawEd25519Point", "KholawEd25519PublicKey", "KholawEd25519PrivateKey",
+    "SLIP10Ed25519Point", "SLIP10Ed25519PublicKey", "SLIP10Ed25519PrivateKey",
+    "SLIP10Ed25519Blake2bPoint", "SLIP10Ed25519Blake2bPublicKey", "SLIP10Ed25519Blake2bPrivateKey",
+    "SLIP10Ed25519MoneroPoint", "SLIP10Ed25519MoneroPublicKey", "SLIP10Ed25519MoneroPrivateKey",
+    "SLIP10Nist256p1Point", "SLIP10Nist256p1PublicKey", "SLIP10Nist256p1PrivateKey",
+    "SLIP10Secp256k1Point", "SLIP10Secp256k1PublicKey", "SLIP10Secp256k1PrivateKey",
     "ECCS", "validate_and_get_public_key"
+] + [
+    ecc.__name__ for ecc in ECCS.values()
 ]
