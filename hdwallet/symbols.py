ff215432d61838d948aaa9f7ee2f04ec66e0f8e8
meherett <meherett.batu@gmail.com>
2024-03-17 12:54:54 +0300

    Update: available hdwallet symbols

--- hdwallet/symbols.py
+++ hdwallet/symbols.py
@@ -6,10 +6,18 @@
 
 from typing import List
 
+# Adcoin
+ACC: str = "ACC"
+# Akash-Network
+AKT: str = "AKT"
 # Algorand
 ALGO: str = "ALGO"
 # Anon
 ANON: str = "ANON"
+# Aptos
+APT: str = "APT"
+# Arbitrum
+ARB: str = "ARB"
 # Argoneum
 AGM: str = "AGM"
 # Artax
@@ -18,30 +26,44 @@ XAX: str = "XAX"
 AYA: str = "AYA"
 # Asiacoin
 AC: str = "AC"
-# Atom
-ATOM: str = "ATOM"
 # Auroracoin
 AUR: str = "AUR"
+# Avalanche
+AVAX: str = "AVAX"
 # Avian
 AVN: str = "AVN"
 # Axe
 AXE: str = "AXE"
+# Axelar
+AXL: str = "AXL"
+# Band-Protocol
+BAND: str = "BAND"
 # Bata
 BTA: str = "BTA"
 # Beetle Coin
 BEET: str = "BEET"
 # Bela Coin
 BELA: str = "BELA"
+# Binance
+BNB: str = "BNB"
 # Bit Cloud
 BTDX: str = "BTDX"
 # Bitcoin
 BTC: str = "BTC"
-# Bitcoin Cash
+# Bitcoin Atom
+BCA: str = "BCA"
+# Bitcoin-Cash
 BCH: str = "BCH"
+# Bitcoin-Cash-SLP
+SLP: str = "SLP"
 # Bitcoin Gold
 BTG: str = "BTG"
+# Bitcoin-Green
+BITG: str = "BITG"
 # Bitcoin Plus
 XBC: str = "XBC"
+# Bitcoin-Private
+BTCP: str = "BTCP"
 # Bitcoin SV
 BSV: str = "BSV"
 # BitcoinZ
@@ -66,12 +88,18 @@ CDN: str = "CDN"
 CCN: str = "CCN"
 # Cardano
 ADA: str = "ADA"
+# Celo
+CELO: str = "CELO"
+# Chihuahua
+HUA: str = "HUA"
 # Clams
 CLAM: str = "CLAM"
 # Club Coin
 CLUB: str = "CLUB"
 # Compcoin
 CMP: str = "CMP"
+# Cosmos
+ATOM: str = "ATOM"
 # CPU Chain
 CPU: str = "CPU"
 # Crane Pay
@@ -92,20 +120,28 @@ DMD: str = "DMD"
 DGB: str = "DGB"
 # Digitalcoin
 DGC: str = "DGC"
+# Divi
+DIVI: str = "DIVI"
 # Dogecoin
 DOGE: str = "DOGE"
+# eCash
+XEC: str = "XEC"
 # E-coin
 ECN: str = "ECN"
 # EDR Coin
 EDRC: str = "EDRC"
+# e-Gulden
+EFL: str = "EFL"
 # Einsteinium
 EMC2: str = "EMC2"
 # Elastos
 ELA: str = "ELA"
-# Elrond or MultiversX
-EGLD: str = "EGLD"
 # Energi
 NRG: str = "NRG"
+# EOS
+EOS: str = "EOS"
+# Ergo
+ERG: str = "ERG"
 # Ethereum
 ETH: str = "ETH"
 # Europe Coin
@@ -114,8 +150,16 @@ ERC: str = "ERC"
 EVR: str = "EVR"
 # Exclusive Coin
 EXCL: str = "EXCL"
+# Fantom
+FTM: str = "FTM"
 # Feathercoin
 FTC: str = "FTC"
+# Fetch.ai
+FET: str = "FET"
+# Filecoin
+FIL: str = "FIL"
+# Firo
+FIRO: str = "FIRO"
 # Firstcoin
 FRST: str = "FRST"
 # FIX
@@ -140,24 +184,40 @@ GRC: str = "GRC"
 GRS: str = "GRS"
 # Gulden
 NLG: str = "NLG"
+# Harmony
+ONE: str = "ONE"
 # Helleniccoin
 HNC: str = "HNC"
 # Hempcoin
 THC: str = "THC"
+# Horizen
+ZEN: str = "ZEN"
+# Huobi Token
+HT: str = "HT"
 # Hush
 HUSH: str = "HUSH"
+# ICON
+ICX: str = "ICX"
+# Injective
+INJ: str = "INJ"
 # InsaneCoin
 INSN: str = "INSN"
 # Internet Of People
 IOP: str = "IOP"
+# IRISnet
+IRIS: str = "IRIS"
 # IX Coin
 IXC: str = "IXC"
 # Jumbucks
 JBS: str = "JBS"
+# Kava
+KAVA: str = "KAVA"
 # Kobocoin
 KOBO: str = "KOBO"
 # Komodo
 KMD: str = "KMD"
+# Landcoin
+LDCN: str = "LDCN"
 # LBRY Credits
 LBC: str = "LBC"
 # Linx
@@ -176,20 +236,28 @@ LYNX: str = "LYNX"
 MZC: str = "MZC"
 # Megacoin
 MEC: str = "MEC"
+# Metis
+METIS: str = "METIS"
 # Minexcoin
 MNX: str = "MNX"
 # Monacoin
 MONA: str = "MONA"
 # Monero
 XMR: str = "XMR"
-# Monkey Project or Monk
+# Monk
 MONK: str = "MONK"
+# MultiversX
+EGLD: str = "EGLD"
 # Myriadcoin
 XMY: str = "XMY"
 # Namecoin
 NMC: str = "NMC"
+# Nano
+XNO: str = "XNO"
 # Navcoin
 NAV: str = "NAV"
+# Near
+NEAR: str = "NEAR"
 # Neblio
 NEBL: str = "NEBL"
 # Neo
@@ -200,6 +268,8 @@ NEOS: str = "NEOS"
 NRO: str = "NRO"
 # New York Coin
 NYC: str = "NYC"
+# Nine-Chronicles
+NCG: str = "NCG"
 # NIX
 NIX: str = "NIX"
 # Novacoin
@@ -210,20 +280,34 @@ NBT: str = "NBT"
 NSR: str = "NSR"
 # OK Cash
 OK: str = "OK"
+# OKT-Chain
+OKT: str = "OKT"
 # Omni
 OMNI: str = "OMNI"
 # Onix
 ONX: str = "ONX"
+# Ontology
+ONT: str = "ONT"
+# Optimism
+OP: str = "OP"
+# Osmosis
+OSMO: str = "OSMO"
+# Particl
+PART: str = "PART"
 # Peercoin
 PPC: str = "PPC"
 # Pesobit
 PSB: str = "PSB"
 # Phore
 PHR: str = "PHR"
+# Pi-Network
+PI: str = "PI"
 # Pinkcoin
 PINK: str = "PINK"
 # Pivx
 PIVX: str = "PIVX"
+# Polygon
+MATIC: str = "MATIC"
 # PoSW Coin
 POSW: str = "POSW"
 # Potcoin
@@ -242,6 +326,8 @@ RVN: str = "RVN"
 RDD: str = "RDD"
 # Ripple
 XRP: str = "XRP"
+# Ritocoin
+RITO: str = "RITO"
 # RSK
 RBTC: str = "RBTC"
 # Rubycoin
@@ -252,8 +338,12 @@ SAFE: str = "SAFE"
 SLS: str = "SLS"
 # Scribe
 SCRIBE: str = "SCRIBE"
+# Secret
+SCRT: str = "SCRT"
 # Shadow Cash
 SDC: str = "SDC"
+# Shentu
+CTK: str = "CTK"
 # Slimcoin
 SLM: str = "SLM"
 # Smileycoin
@@ -262,6 +352,8 @@ SMLY: str = "SMLY"
 SOL: str = "SOL"
 # Solarcoin
 SLR: str = "SLR"
+# Stafi
+FIS: str = "FIS"
 # Stash
 STASH: str = "STASH"
 # Stellar
@@ -270,10 +362,16 @@ XLM: str = "XLM"
 STRAT: str = "STRAT"
 # Sugarchain
 SUGAR: str = "SUGAR"
+# Sui
+SUI: str = "SUI"
 # Syscoin
 SYS: str = "SYS"
+# Terra
+LUNA: str = "LUNA"
 # Tezos
 XTZ: str = "XTZ"
+# Theta
+THETA: str = "THETA"
 # Thought AI
 THT: str = "THT"
 # TOA Coin
@@ -288,6 +386,8 @@ USC: str = "USC"
 UNO: str = "UNO"
 # Vcash
 VC: str = "VC"
+# VeChain
+VET: str = "VET"
 # Verge
 XVG: str = "XVG"
 # Vertcoin
@@ -296,8 +396,12 @@ VTC: str = "VTC"
 VIA: str = "VIA"
 # Vivo
 VIVO: str = "VIVO"
+# Voxels
+VOX: str = "VOX"
 # Virtual Cash
 VASH: str = "VASH"
+# Wagerr
+WGR: str = "WGR"
 # Whitecoin
 XWC: str = "XWC"
 # Wincoin
@@ -312,28 +416,43 @@ YEC: str = "YEC"
 ZEC: str = "ZEC"
 # ZClassic
 ZCL: str = "ZCL"
-# Zencash or Horizen
-ZEN: str = "ZEN"
+# Zetacoin
+ZET: str = "ZET"
+# Zilliqa
+ZIL: str = "ZIL"
+# ZooBC
+ZBC: str = "ZBC"
 
 __all__: List[str] = [
+    "ACC",
+    "AKT",
     "ALGO",
     "ANON",
+    "APT",
+    "ARB",
     "AGM",
     "XAX",
     "AYA",
     "AC",
-    "ATOM",
     "AUR",
+    "AVAX",
     "AVN",
     "AXE",
+    "AXL",
+    "BAND",
     "BTA",
     "BEET",
     "BELA",
+    "BNB",
     "BTDX",
     "BTC",
+    "BCA",
     "BCH",
+    "SLP",
     "BTG",
+    "BITG",
     "XBC",
+    "BTCP",
     "BSV",
     "BTCZ",
     "BTX",
@@ -346,9 +465,12 @@ __all__: List[str] = [
     "CDN",
     "CCN",
     "ADA",
+    "CELO",
+    "HUA",
     "CLAM",
     "CLUB",
     "CMP",
+    "ATOM",
     "CPU",
     "CRP",
     "CRAVE",
@@ -359,18 +481,26 @@ __all__: List[str] = [
     "DMD",
     "DGB",
     "DGC",
+    "DIVI",
     "DOGE",
+    "XEC",
     "ECN",
     "EDRC",
+    "EFL",
     "EMC2",
     "ELA",
-    "EGLD",
     "NRG",
+    "EOS",
+    "ERG",
     "ETH",
     "ERC",
     "EVR",
     "EXCL",
+    "FTM",
     "FTC",
+    "FET",
+    "FIL",
+    "FIRO",
     "FRST",
     "FIX",
     "FLASH",
@@ -383,15 +513,23 @@ __all__: List[str] = [
     "GRC",
     "GRS",
     "NLG",
+    "ONE",
     "HNC",
     "THC",
+    "ZEN",
+    "HT",
     "HUSH",
+    "ICX",
+    "INJ",
     "INSN",
     "IOP",
+    "IRIS",
     "IXC",
     "JBS",
+    "KAVA",
     "KOBO",
     "KMD",
+    "LDCN",
     "LBC",
     "LINX",
     "LTC",
@@ -401,30 +539,42 @@ __all__: List[str] = [
     "LYNX",
     "MZC",
     "MEC",
+    "METIS",
     "MNX",
     "MONA",
     "XMR",
     "MONK",
+    "EGLD",
     "XMY",
     "NMC",
+    "XNO",
     "NAV",
+    "NEAR",
     "NEBL",
     "NEO",
     "NEOS",
     "NRO",
     "NYC",
+    "NCG",
     "NIX",
     "NVC",
     "NBT",
     "NSR",
     "OK",
+    "OKT",
     "OMNI",
     "ONX",
+    "ONT",
+    "OP",
+    "OSMO",
+    "PART",
     "PPC",
     "PSB",
     "PHR",
+    "PI",
     "PINK",
     "PIVX",
+    "MATIC",
     "POSW",
     "POT",
     "PRJ",
@@ -434,22 +584,29 @@ __all__: List[str] = [
     "RVN",
     "RDD",
     "XRP",
+    "RITO",
     "RBTC",
     "RBY",
     "SAFE",
     "SLS",
     "SCRIBE",
+    "SCRT",
     "SDC",
+    "CTK",
     "SLM",
     "SMLY",
     "SOL",
     "SLR",
+    "FIS",
     "STASH",
     "XLM",
     "STRAT",
     "SUGAR",
+    "SUI",
     "SYS",
+    "LUNA",
     "XTZ",
+    "THETA",
     "THT",
     "TOA",
     "TRX",
@@ -457,11 +614,14 @@ __all__: List[str] = [
     "USC",
     "UNO",
     "VC",
+    "VET",
     "XVG",
     "VTC",
     "VIA",
     "VIVO",
+    "VOX",
     "VASH",
+    "WGR",
     "XWC",
     "WC",
     "XDC",
@@ -469,5 +629,7 @@ __all__: List[str] = [
     "YEC",
     "ZEC",
     "ZCL",
-    "ZEN"
+    "ZET",
+    "ZIL",
+    "ZBC"
 ]
