9505749edd2dc5ef045a565d3b26baa4407953d7
meherett <meherett.batu@gmail.com>
2024-03-31 15:51:43 +0300

    Update: default values of CLI entropy & mnemonic

--- hdwallet/cli/generate/entropy.py
+++ hdwallet/cli/generate/entropy.py
@@ -4,19 +4,40 @@
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or https://opensource.org/license/mit
 
+from typing import Optional
+
 import click
 import sys
 
-from ...entropies import ENTROPIES
+from ...entropies import (
+    AlgorandEntropy, ALGORAND_ENTROPY_STRENGTHS,
+    BIP39Entropy, BIP39_ENTROPY_STRENGTHS,
+    ElectrumV1Entropy, ELECTRUM_V1_ENTROPY_STRENGTHS,
+    ElectrumV2Entropy, ELECTRUM_V2_ENTROPY_STRENGTHS,
+    MoneroEntropy, MONERO_ENTROPY_STRENGTHS,
+    ENTROPIES
+)
 
 
-def generate_entropy(name: str, strength: int) -> None:
+def generate_entropy(name: str, strength: Optional[int]) -> None:
     try:
         if name not in ENTROPIES.keys():
             click.echo(click.style(
                 f"Wrong entropy name, (expected={list(ENTROPIES.keys())}, got='{name}')"
             ), err=True)
             sys.exit()
+
+        if strength is None:  # Set default strength
+            if name == AlgorandEntropy.name():
+                strength = ALGORAND_ENTROPY_STRENGTHS.TWO_HUNDRED_FIFTY_SIX
+            elif name == BIP39Entropy.name():
+                strength = BIP39_ENTROPY_STRENGTHS.ONE_HUNDRED_TWENTY_EIGHT
+            elif name == ElectrumV1Entropy.name():
+                strength = ELECTRUM_V1_ENTROPY_STRENGTHS.ONE_HUNDRED_TWENTY_EIGHT
+            elif name == ElectrumV2Entropy.name():
+                strength = ELECTRUM_V2_ENTROPY_STRENGTHS.ONE_HUNDRED_THIRTY_TWO
+            elif name == MoneroEntropy.name():
+                strength = MONERO_ENTROPY_STRENGTHS.ONE_HUNDRED_TWENTY_EIGHT
 
         if not ENTROPIES[name].is_valid_strength(strength=strength):
             click.echo(click.style(
--- hdwallet/cli/generate/mnemonic.py
+++ hdwallet/cli/generate/mnemonic.py
@@ -4,13 +4,22 @@
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or https://opensource.org/license/mit
 
+from typing import Optional
+
 import click
 import sys
 
-from ...mnemonics import MNEMONICS
+from ...mnemonics import (
+    AlgorandMnemonic, ALGORAND_MNEMONIC_WORDS, ALGORAND_MNEMONIC_LANGUAGES,
+    BIP39Mnemonic, BIP39_MNEMONIC_WORDS, BIP39_MNEMONIC_LANGUAGES,
+    ElectrumV1Mnemonic, ELECTRUM_V1_MNEMONIC_WORDS, ELECTRUM_V1_MNEMONIC_LANGUAGES,
+    ElectrumV2Mnemonic, ELECTRUM_V2_MNEMONIC_WORDS, ELECTRUM_V2_MNEMONIC_LANGUAGES,
+    MoneroMnemonic, MONERO_MNEMONIC_WORDS, MONERO_MNEMONIC_LANGUAGES,
+    MNEMONICS
+)
 
 
-def generate_mnemonic(name: str, language: str, words: int) -> None:
+def generate_mnemonic(name: str, language: Optional[str], words: Optional[str]) -> None:
     try:
         if name not in MNEMONICS.keys():
             click.echo(click.style(
@@ -18,19 +27,45 @@ def generate_mnemonic(name: str, language: str, words: int) -> None:
             ), err=True)
             sys.exit()
 
-        if language not in MNEMONICS[name].languages:
+        if language is None:  # Set default language
+            if name == AlgorandMnemonic.name():
+                language = ALGORAND_MNEMONIC_LANGUAGES.ENGLISH
+            elif name == BIP39Mnemonic.name():
+                language = BIP39_MNEMONIC_LANGUAGES.ENGLISH
+            elif name == ElectrumV1Mnemonic.name():
+                language = ELECTRUM_V1_MNEMONIC_LANGUAGES.ENGLISH
+            elif name == ElectrumV2Mnemonic.name():
+                language = ELECTRUM_V2_MNEMONIC_LANGUAGES.ENGLISH
+            elif name == MoneroMnemonic.name():
+                language = MONERO_MNEMONIC_LANGUAGES.ENGLISH
+
+        if words is None:  # Set default words
+            if name == AlgorandMnemonic.name():
+                words = ALGORAND_MNEMONIC_WORDS.TWENTY_FIVE
+            elif name == BIP39Mnemonic.name():
+                words = BIP39_MNEMONIC_WORDS.TWELVE
+            elif name == ElectrumV1Mnemonic.name():
+                words = ELECTRUM_V1_MNEMONIC_WORDS.TWELVE
+            elif name == ElectrumV2Mnemonic.name():
+                words = ELECTRUM_V2_MNEMONIC_WORDS.TWELVE
+            elif name == MoneroMnemonic.name():
+                words = MONERO_MNEMONIC_WORDS.TWELVE
+
+        if not MNEMONICS[name].is_valid_language(language=language):
             click.echo(click.style(
                 f"Wrong {name} mnemonic language, (expected={MNEMONICS[name].languages}, got='{language}')"
             ), err=True)
             sys.exit()
 
-        if words not in MNEMONICS[name].words:
+        if not MNEMONICS[name].is_valid_words(words=words):
             click.echo(click.style(
                 f"Wrong {name} mnemonic words, (expected={MNEMONICS[name].words}, got='{words}')"
             ), err=True)
             sys.exit()
 
-        click.echo(MNEMONICS[name].from_words(words=words, language=language))
+        click.echo(MNEMONICS[name].from_words(
+            words=words, language=language
+        ))
 
     except Exception as exception:
         click.echo(click.style(f"Error: {str(exception)}"), err=True)
