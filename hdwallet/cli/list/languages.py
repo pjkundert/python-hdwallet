726b6c498d8bc1015d564dc47f648b7c87af438d
meherett <meherett.batu@gmail.com>
2024-04-11 10:48:14 +0300

    Add: languages CLI on list command

--- hdwallet/cli/list/languages.py
+++ hdwallet/cli/list/languages.py
@@ -1,25 +1,35 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+
+# Copyright © 2020-2024, Meheret Tesfaye Batu <meherett.batu@gmail.com>
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or https://opensource.org/license/mit
 
 from tabulate import tabulate
 
-from hdwallet.cli import click
+import click
+
+from ...mnemonics import MNEMONICS
 
 
 def list_languages():
 
-    click.echo(tabulate(
-        [
-            ["Chinese Simplified"],
-            ["Chinese Traditional"],
-            ["English"],
-            ["French"],
-            ["Italian"],
-            ["Japanese"],
-            ["Korean"],
-            ["Spanish"],
-        ],
-        [
-            "Language"
-        ],
-        tablefmt="github"
-    ))
+    for i, mnemonic in enumerate(MNEMONICS.values()):
+
+        languages: list = []
+        for _language in mnemonic.languages:
+            language: str = ""
+            for index, _ in enumerate(_language.split("-")):
+                language += _.title() if index == 0 else f"-{_.title()}"
+            languages.append([language])
+
+        click.echo(tabulate(
+            languages,
+            [
+                f"{mnemonic.name()} Languages"
+            ],
+            tablefmt="github",
+            stralign="left",
+            numalign="left"
+        ))
+        if len(MNEMONICS.keys()) - 1 != i:
+            click.echo("\n")
