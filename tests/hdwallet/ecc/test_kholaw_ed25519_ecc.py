2df3f23484e9f1c9645ba27c6b358859f821e39b
axiom <axiom90@proton.me>
2024-06-26 17:52:43 +0300

    Add: All ECC Tests

---
+++ tests/hdwallet/ecc/test_kholaw_ed25519_ecc.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python3
+
+# Copyright © 2020-2024, Meheret Tesfaye Batu <meherett.batu@gmail.com>
+#             2024, Eyoel Tadesse <eyoel_tadesse@proton.me>
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or https://opensource.org/license/mit
+
+from nacl.signing import (
+    SigningKey, VerifyKey
+)
+
+import json
+import os
+import pytest
+
+from hdwallet.ecc import (
+    IPoint, IPublicKey, IPrivateKey
+)
+from hdwallet.ecc.kholaw.ed25519 import (
+    KholawEd25519ECC, KholawEd25519Point, KholawEd25519PublicKey, KholawEd25519PrivateKey
+)
+from hdwallet.utils import get_bytes
+
+# Test Values
+base_path: str = os.path.dirname(__file__)
+file_path: str = os.path.abspath(os.path.join(base_path, "../../data/eccs.json"))
+values = open(file_path, "r", encoding="utf-8")
+_: dict = json.loads(values.read())
+values.close()
+
+
+def test_kholaw_ed25519_ecc():
+
+    assert KholawEd25519ECC.NAME == _["Kholaw-Ed25519"]["name"]
+    assert isinstance(KholawEd25519ECC.ORDER, int)
+    assert isinstance(KholawEd25519ECC.GENERATOR, IPoint)
+    assert isinstance(KholawEd25519ECC.POINT, type(KholawEd25519Point))
+    assert isinstance(KholawEd25519ECC.PUBLIC_KEY, type(KholawEd25519PublicKey))
+    assert isinstance(KholawEd25519ECC.PRIVATE_KEY, type(KholawEd25519PrivateKey))
+
+
+def test_kholaw_ed25519_ecc_point():
+
+    assert KholawEd25519Point.name() == _["Kholaw-Ed25519"]["name"]
+    for public_key_type in ["uncompressed", "compressed"]:
+        # Test from bytes
+        point = KholawEd25519Point.from_bytes(
+            get_bytes(_["Kholaw-Ed25519"][public_key_type]["point"]["encode"])
+        )
+        assert isinstance(point, IPoint)
+        assert isinstance(point, KholawEd25519Point)
+        assert isinstance(point.underlying_object(), bytes)
+        assert point.x() == _["Kholaw-Ed25519"][public_key_type]["point"]["x"]
+        assert point.y() == _["Kholaw-Ed25519"][public_key_type]["point"]["y"]
+        assert point.raw_encoded() == get_bytes(_["Kholaw-Ed25519"][public_key_type]["point"]["encode"])
+        assert point.raw() == point.raw_decoded() == get_bytes(_["Kholaw-Ed25519"][public_key_type]["point"]["decode"])
+        # Test from coordinate
+        point = KholawEd25519Point.from_coordinates(
+            x=_["Kholaw-Ed25519"][public_key_type]["point"]["x"],
+            y=_["Kholaw-Ed25519"][public_key_type]["point"]["y"]
+        )
+        assert isinstance(point, IPoint)
+        assert isinstance(point, KholawEd25519Point)
+        assert isinstance(point.underlying_object(), bytes)
+        assert point.x() == _["Kholaw-Ed25519"][public_key_type]["point"]["x"]
+        assert point.y() == _["Kholaw-Ed25519"][public_key_type]["point"]["y"]
+        assert point.raw_encoded() == get_bytes(_["Kholaw-Ed25519"][public_key_type]["point"]["encode"])
+        assert point.raw() == point.raw_decoded() == get_bytes(_["Kholaw-Ed25519"][public_key_type]["point"]["decode"])
+
+        for number in range(2, 50):
+            point_add, point_radd, point_mul, point_rmul = (
+                point + (point * (number - 1)), (point * (number - 1)) + point, point * number, number * point
+            )
+            assert point_add.raw() == point_radd.raw() == point_mul.raw() == point_rmul.raw()
+            assert point_add.raw_encoded() == point_radd.raw_encoded() == point_mul.raw_encoded() == point_rmul.raw_encoded()
+            assert point_add.raw_decoded() == point_radd.raw_decoded() == point_mul.raw_decoded() == point_rmul.raw_decoded()
+            assert point_add.x() == point_radd.x() == point_mul.x() == point_rmul.x()
+            assert point_add.y() == point_radd.y() == point_mul.y() == point_rmul.y()
+
+
+def test_kholaw_ed25519_ecc_public_key():
+
+    assert KholawEd25519PublicKey.name() == _["Kholaw-Ed25519"]["name"]
+    assert KholawEd25519PublicKey.uncompressed_length() == _["Kholaw-Ed25519"]["uncompressed"]["length"]
+    assert KholawEd25519PublicKey.compressed_length() == _["Kholaw-Ed25519"]["compressed"]["length"]
+    for public_key_type in ["uncompressed", "compressed"]:
+        public_key = KholawEd25519PublicKey.from_bytes(
+            get_bytes(_["Kholaw-Ed25519"][public_key_type]["public-key"])
+        )
+        assert isinstance(public_key, IPublicKey)
+        assert isinstance(public_key, KholawEd25519PublicKey)
+        assert isinstance(public_key.underlying_object(), VerifyKey)
+        assert public_key.raw_uncompressed() == get_bytes(_["Kholaw-Ed25519"]["uncompressed"]["public-key"])
+        assert public_key.raw_compressed() == get_bytes(_["Kholaw-Ed25519"]["compressed"]["public-key"])
+        assert isinstance(public_key.point(), IPoint)
+        assert isinstance(public_key.point(), KholawEd25519Point)
+
+
+def test_kholaw_ed25519_ecc_private_key():
+
+    assert KholawEd25519PrivateKey.name() == _["Kholaw-Ed25519"]["name"]
+    assert KholawEd25519PrivateKey.length() == _["Kholaw-Ed25519"]["private-key-length"]
+    private_key = KholawEd25519PrivateKey.from_bytes(
+        get_bytes(_["Kholaw-Ed25519"]["private-key"])
+    )
+    assert isinstance(private_key, IPrivateKey)
+    assert isinstance(private_key, KholawEd25519PrivateKey)
+    assert isinstance(private_key.underlying_object(), SigningKey)
+    assert isinstance(private_key.raw(), bytes)
+    assert private_key.raw() == get_bytes(_["Kholaw-Ed25519"]["private-key"])
+    assert isinstance(private_key.public_key(), IPublicKey)
+    assert isinstance(private_key.public_key(), KholawEd25519PublicKey)
+    assert private_key.public_key().raw_uncompressed() == get_bytes(_["Kholaw-Ed25519"]["uncompressed"]["public-key"])
+    assert private_key.public_key().raw_compressed() == get_bytes(_["Kholaw-Ed25519"]["compressed"]["public-key"])
---
+++ tests/hdwallet/ecc/test_slip10_nist256p1_ecc.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python3
+
+# Copyright © 2020-2024, Meheret Tesfaye Batu <meherett.batu@gmail.com>
+#             2024, Eyoel Tadesse <eyoel_tadesse@proton.me>
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or https://opensource.org/license/mit
+
+from ecdsa import (
+    SigningKey, VerifyingKey
+)
+from ecdsa.ellipticcurve import PointJacobi
+
+import json
+import os
+import pytest
+
+from hdwallet.ecc import (
+    IPoint, IPublicKey, IPrivateKey
+)
+from hdwallet.ecc.slip10.nist256p1 import (
+    SLIP10Nist256p1ECC, SLIP10Nist256p1Point, SLIP10Nist256p1PublicKey, SLIP10Nist256p1PrivateKey
+)
+from hdwallet.utils import get_bytes
+
+# Test Values
+base_path: str = os.path.dirname(__file__)
+file_path: str = os.path.abspath(os.path.join(base_path, "../../data/eccs.json"))
+values = open(file_path, "r", encoding="utf-8")
+_: dict = json.loads(values.read())
+values.close()
+
+
+def test_slip10_nist256p1_ecc():
+
+    assert SLIP10Nist256p1ECC.NAME == _["SLIP10-Nist256p1"]["name"]
+    assert isinstance(SLIP10Nist256p1ECC.ORDER, int)
+    assert isinstance(SLIP10Nist256p1ECC.GENERATOR, IPoint)
+    assert isinstance(SLIP10Nist256p1ECC.POINT, type(SLIP10Nist256p1Point))
+    assert isinstance(SLIP10Nist256p1ECC.PUBLIC_KEY, type(SLIP10Nist256p1PublicKey))
+    assert isinstance(SLIP10Nist256p1ECC.PRIVATE_KEY, type(SLIP10Nist256p1PrivateKey))
+
+
+def test_slip10_nist256p1_ecc_point():
+
+    assert SLIP10Nist256p1Point.name() == _["SLIP10-Nist256p1"]["name"]
+    for public_key_type in ["uncompressed", "compressed"]:
+        # Test from bytes
+        point = SLIP10Nist256p1Point.from_bytes(
+            get_bytes(_["SLIP10-Nist256p1"][public_key_type]["point"]["encode"])
+        )
+        assert isinstance(point, IPoint)
+        assert isinstance(point, SLIP10Nist256p1Point)
+        assert isinstance(point.underlying_object(), PointJacobi)
+        assert point.x() == _["SLIP10-Nist256p1"][public_key_type]["point"]["x"]
+        assert point.y() == _["SLIP10-Nist256p1"][public_key_type]["point"]["y"]
+        assert point.raw_encoded() == get_bytes(_["SLIP10-Nist256p1"][public_key_type]["point"]["encode"])
+        assert point.raw() == point.raw_decoded() == get_bytes(_["SLIP10-Nist256p1"][public_key_type]["point"]["decode"])
+        # Test from coordinate
+        point = SLIP10Nist256p1Point.from_coordinates(
+            x=_["SLIP10-Nist256p1"][public_key_type]["point"]["x"],
+            y=_["SLIP10-Nist256p1"][public_key_type]["point"]["y"]
+        )
+        assert isinstance(point, IPoint)
+        assert isinstance(point, SLIP10Nist256p1Point)
+        assert isinstance(point.underlying_object(), PointJacobi)
+        assert point.x() == _["SLIP10-Nist256p1"][public_key_type]["point"]["x"]
+        assert point.y() == _["SLIP10-Nist256p1"][public_key_type]["point"]["y"]
+        assert point.raw_encoded() == get_bytes(_["SLIP10-Nist256p1"][public_key_type]["point"]["encode"])
+        assert point.raw() == point.raw_decoded() == get_bytes(_["SLIP10-Nist256p1"][public_key_type]["point"]["decode"])
+
+        for number in range(2, 50):
+            point_add, point_radd, point_mul, point_rmul = (
+                point + (point * (number - 1)), (point * (number - 1)) + point, point * number, number * point
+            )
+            assert point_add.raw() == point_radd.raw() == point_mul.raw() == point_rmul.raw()
+            assert point_add.raw_encoded() == point_radd.raw_encoded() == point_mul.raw_encoded() == point_rmul.raw_encoded()
+            assert point_add.raw_decoded() == point_radd.raw_decoded() == point_mul.raw_decoded() == point_rmul.raw_decoded()
+            assert point_add.x() == point_radd.x() == point_mul.x() == point_rmul.x()
+            assert point_add.y() == point_radd.y() == point_mul.y() == point_rmul.y()
+
+
+def test_slip10_nist256p1_ecc_public_key():
+
+    assert SLIP10Nist256p1PublicKey.name() == _["SLIP10-Nist256p1"]["name"]
+    assert SLIP10Nist256p1PublicKey.uncompressed_length() == _["SLIP10-Nist256p1"]["uncompressed"]["length"]
+    assert SLIP10Nist256p1PublicKey.compressed_length() == _["SLIP10-Nist256p1"]["compressed"]["length"]
+    for public_key_type in ["uncompressed", "compressed"]:
+        public_key = SLIP10Nist256p1PublicKey.from_bytes(
+            get_bytes(_["SLIP10-Nist256p1"][public_key_type]["public-key"])
+        )
+        assert isinstance(public_key, IPublicKey)
+        assert isinstance(public_key, SLIP10Nist256p1PublicKey)
+        assert isinstance(public_key.underlying_object(), VerifyingKey)
+        assert public_key.raw_uncompressed() == get_bytes(_["SLIP10-Nist256p1"]["uncompressed"]["public-key"])
+        assert public_key.raw_compressed() == get_bytes(_["SLIP10-Nist256p1"]["compressed"]["public-key"])
+        assert isinstance(public_key.point(), IPoint)
+        assert isinstance(public_key.point(), SLIP10Nist256p1Point)
+
+
+def test_slip10_nist256p1_ecc_private_key():
+
+    assert SLIP10Nist256p1PrivateKey.name() == _["SLIP10-Nist256p1"]["name"]
+    assert SLIP10Nist256p1PrivateKey.length() == _["SLIP10-Nist256p1"]["private-key-length"]
+    private_key = SLIP10Nist256p1PrivateKey.from_bytes(
+        get_bytes(_["SLIP10-Nist256p1"]["private-key"])
+    )
+    assert isinstance(private_key, IPrivateKey)
+    assert isinstance(private_key, SLIP10Nist256p1PrivateKey)
+    assert isinstance(private_key.underlying_object(), SigningKey)
+    assert isinstance(private_key.raw(), bytes)
+    assert private_key.raw() == get_bytes(_["SLIP10-Nist256p1"]["private-key"])
+    assert isinstance(private_key.public_key(), IPublicKey)
+    assert isinstance(private_key.public_key(), SLIP10Nist256p1PublicKey)
+    assert private_key.public_key().raw_uncompressed() == get_bytes(_["SLIP10-Nist256p1"]["uncompressed"]["public-key"])
+    assert private_key.public_key().raw_compressed() == get_bytes(_["SLIP10-Nist256p1"]["compressed"]["public-key"])
