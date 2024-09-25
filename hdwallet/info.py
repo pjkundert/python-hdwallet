528256f242d6489d93018eb270f695b5b0137949
axiom <axiom90@proton.me>
2024-09-25 12:01:37 +0300

    Bump: hdwallet-desktop 0.1.0b6

--- hdwallet/info.py
+++ hdwallet/info.py
@@ -9,7 +9,7 @@ from typing import List
 __name__: str = "hdwallet"
 __versions__: dict = {
     __name__: "v3.0.0a10",
-    "desktop": "v0.1.0b5"
+    "desktop": "v0.1.0b6"
 }
 __license__: str = "MIT"
 __author__: str = "Meheret Tesfaye Batu"
