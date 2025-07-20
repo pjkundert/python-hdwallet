eb81407db95897d6e08b6c1056065d0db51593d9
Perry Kundert <perry@dominionrnd.com>
2025-07-20 10:02:06 -0600

    Test BIP-39 and raw hex SLIP-39 seeds

--- tests/hdwallet/seeds/test_seeds_slip39.py
+++ tests/hdwallet/seeds/test_seeds_slip39.py
@@ -19,6 +19,12 @@ def test_slip39_seeds(data):
     for words in data["seeds"]["SLIP39"].keys():
         for lang in data["seeds"]["SLIP39"][words].keys():
             mnemonic = data["seeds"]["SLIP39"][words][lang]["mnemonic"]
+            # A SLIP-39 "backup" for another Mnemonic (eg. BIP-39) backs up the original entropy
+            # (not the derived seed, which will often be stretched to a different length).  SLIP-39
+            # /can/ store 512-bit data (the output of a BIP-39 seed, after hashing it with its
+            # passphrase, but this is not generally supported by hardware wallets supporting
+            # SLIP-39, such as the Trezor.  This is unfortunate, as it prevents backing up BIP-39
+            # derived seeds including the passphrase.
             try:
                 mnemonic = BIP39Mnemonic(mnemonic=mnemonic)
             except Exception:
@@ -26,5 +32,5 @@ def test_slip39_seeds(data):
                 pass
             assert SLIP39Seed.from_mnemonic(
                 mnemonic = mnemonic
-            ) == data["seeds"]["SLIP39"][words][lang]["seed"]
+            ) == data["seeds"]["SLIP39"][words][lang]["non-passphrase-seed"]
 
